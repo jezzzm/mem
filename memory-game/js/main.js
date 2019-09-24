7990267093824d9df2be2677c73433626b082693
Jeremy Milledge <jez.milledge@gmail.com>
2019-09-24 13:06:41 +1000

    added variables and match logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+
+const cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne, cardTwo);
+
+console.log(cardsInPlay);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('Match found!');
+	} else {
+		alert('No match');
+	}
+}
