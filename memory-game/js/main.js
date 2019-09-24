ab69b697385d8ee9cfd247994577f1946dfdd085
Jeremy Milledge <jez.milledge@gmail.com>
2019-09-24 15:53:36 +1000

    possible cards now contained in object const

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,11 +1,33 @@
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		image: "images/queen-of-hearts.jpg"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		image: "images/queen-of-diamonds.jpg"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		image: "images/king-of-hearts.jpg"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		image: "images/king-of-diamonds.jpg"
+	}
+];
 
-
-const cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].image);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
